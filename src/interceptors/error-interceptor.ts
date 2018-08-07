1f923701520d9bf76b74def60bf37e4631dea788
Leocaliban <leocaliban@gmail.com>
2018-08-07 16:12:40 -0300

    Trata erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,3 +1,4 @@
+import { StorageService } from './../services/storage.service';
 import { Injectable } from "../../node_modules/@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "../../node_modules/@angular/common/http";
 import { Observable } from "../../node_modules/rxjs";
@@ -5,6 +6,11 @@ import { Observable } from "../../node_modules/rxjs";
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
+  constructor(public storage: StorageService) {
+
+  }
+
+  //Intercepta e encaminha as requisições normalmente, caso ocorra algum erro na requisição o erro é propagado ao controlador
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).catch((error, caught) => {
       let errorObj = error;
@@ -17,8 +23,20 @@ export class ErrorInterceptor implements HttpInterceptor {
 
       console.log("Erro capturado pelo interceptor: ");
       console.log(errorObj);
+
+      switch (errorObj.status) {
+        case 403:
+          this.handle403();
+          break;
+      }
+
+
       return Observable.throw(error);
     }) as any;
+  }
+
+  handle403() {
+    this.storage.setLocalUser(null);
   }
 }
 
--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -29,15 +29,22 @@ export class PerfilPage {
         this.buscarImagemSeExistir();
 
       },
-        error => { });
+        error => {
+          if (error.status == 403) {//Se der erro 403, enviar o usuário para a página de login
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    }
+    else{
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
-  buscarImagemSeExistir(){
-    this.clienteService.buscarImagemDoBucket(this.cliente.id).subscribe(response =>{
+  buscarImagemSeExistir() {
+    this.clienteService.buscarImagemDoBucket(this.cliente.id).subscribe(response => {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseURL}/cp${this.cliente.id}.jpg`;
     },
-    error => { });
+      error => { });
   }
 
 }
