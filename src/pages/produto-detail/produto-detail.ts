16c45c9af5babeb92668a587198fcaaca563c033
Leocaliban <leocaliban@gmail.com>
2018-08-11 04:49:37 -0300

    Cria a página de ProdutoDetail

---
+++ src/pages/produto-detail/produto-detail.ts
@@ -0,0 +1,25 @@
+import { ProdutoDTO } from './../../models/produto.dro';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-produto-detail',
+  templateUrl: 'produto-detail.html',
+})
+export class ProdutoDetailPage {
+
+  item: ProdutoDTO;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.item = {
+      id: '1',
+      nome: 'Mouse',
+      valor: 80.59
+    }
+  }
+
+}
