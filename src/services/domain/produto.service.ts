9330c715d6c0dba996f4f66d4a38af34e8d3b5de
Leocaliban <leocaliban@gmail.com>
2018-08-11 05:30:38 -0300

    Aplica o carregamento dos dados dos produtos na página ProdutoDetail

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -2,6 +2,7 @@ import { API_CONFIG } from './../../config/api.config';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "../../../node_modules/@angular/core";
 import { Observable } from '../../../node_modules/rxjs/Rx';
+import { ProdutoDTO } from '../../models/produto.dro';
 
 @Injectable()
 export class ProdutoService{
@@ -13,8 +14,18 @@ export class ProdutoService{
     return this.http.get(`${API_CONFIG.baseURL}/produtos/?categorias=${categoria_id}`);
   }
 
+  buscarPorId(produto_id : string){
+    return this.http.get<ProdutoDTO>(`${API_CONFIG.baseURL}/produtos/${produto_id}`);
+
+  }
+
   buscarImagemDoBucketSmall(id : string) : Observable<any>{
     let url = `${API_CONFIG.bucketBaseURL}/prod${id}-small.jpg`;
+    return this.http.get(url, {responseType : 'blob'});
+  }
+
+  buscarImagemDoBucket(id : string) : Observable<any>{
+    let url = `${API_CONFIG.bucketBaseURL}/prod${id}.jpg`;
     return this.http.get(url, {responseType : 'blob'});
   }
 
