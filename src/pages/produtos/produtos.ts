68d5372ec7d3c9ccea32fd6076200026699246ea
Leocaliban <leocaliban@gmail.com>
2018-08-12 19:20:41 -0300

    Aplica o componente Loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,6 +1,6 @@
 import { API_CONFIG } from './../../config/api.config';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dro';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -11,34 +11,48 @@ import { ProdutoService } from '../../services/domain/produto.service';
 })
 export class ProdutosPage {
 
-  itens : ProdutoDTO[];
+  itens: ProdutoDTO[];
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,//obtém parâmetros passados na navegação
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get('categoria_id');//recebe os parâmetros passados na página de categorias
+    let loader = this.presentLoading();//abre o loader enquando a requisição reune os dados
     this.produtoService.buscarPorCategoria(categoria_id).subscribe(response => {
       this.itens = response['content'];//na busca paginada do backend, o que queremos obter é o content.
+      loader.dismiss();//fecha o loader após o carregamento dos dados
       this.carregarImagens();
+
     },
-    error => {});
+      error => {
+        loader.dismiss();
+       });
   }
 
-  carregarImagens(){
-    for (var i = 0; i< this.itens.length; i++){
+  carregarImagens() {
+    for (var i = 0; i < this.itens.length; i++) {
       let item = this.itens[i];
       this.produtoService.buscarImagemDoBucketSmall(item.id).subscribe(response => {
         item.imageUrl = `${API_CONFIG.bucketBaseURL}/prod${item.id}-small.jpg`;
       },
-      error => {});
+        error => { });
     }
   }
 
-  showDetail(produto_id : string){
-    this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+  showDetail(produto_id: string) {
+    this.navCtrl.push('ProdutoDetailPage', { produto_id: produto_id });
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Carregando itens..."
+    });
+    loader.present();
+    return loader;
   }
 }
