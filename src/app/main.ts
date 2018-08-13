cd98dca8faf88016df4bc2115a46e5a1d15e31e3
Leocaliban <leocaliban@gmail.com>
2018-08-13 01:34:36 -0300

    Implanta Cordova para tirar foto com a câmera

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,3 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+/**
+ * ionic cordova platform add browser --save
+ * ionic cordova plugin add cordova-plugin-camera
+ * npm install --save @ionic-native/camera
+ */
--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -4,6 +4,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
+import { CameraOptions, Camera } from '../../../node_modules/@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -12,13 +13,16 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class PerfilPage {
 
-  cliente: ClienteDTO;
+  cliente : ClienteDTO;
+  foto : string;
+  cameraOn : boolean = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -35,7 +39,7 @@ export class PerfilPage {
           }
         });
     }
-    else{
+    else {
       this.navCtrl.setRoot('HomePage');
     }
   }
@@ -45,6 +49,26 @@ export class PerfilPage {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseURL}/cp${this.cliente.id}.jpg`;
     },
       error => { });
+  }
+
+  tirarFoto() {
+    this.cameraOn = true;
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      this.foto = 'data:image/png;base64,' + imageData;
+      this.cameraOn = false;
+    }, (err) => {
+      // Handle error
+    });
+
   }
 
 }
